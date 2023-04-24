159a15308ab5f5e782789cebe728f89681ce303a
Huming-bird <sonugaahmed@gmail.com>
2023-04-24 08:16:27 -0700

    sorting algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,43 @@
 #include "sort.h"
 
 /**
- * bubble_sort: This function sorts an array of size 'size'
+ * print_array_elmt - This funtion serves as the putchar for this project
+ *
+ * @elmt: function parameter
+ * Returns: void function
+ */
+
+void print_array_elmt(int elmt)
+{
+	int num_digits = 0;
+	int divisor = 1;
+	int digit;
+
+	if (elmt == 0)
+	{
+		putchar('0');
+		return;
+	}
+
+	while (elmt / divisor != 0)
+	{
+		num_digits++;
+		divisor *= 10;
+	}
+
+	divisor /= 10;
+	while (divisor != 0)
+	{
+		digit = elmt / divisor;
+		putchar(digit + '0');
+		elmt -= digit * divisor;
+		divisor /= 10;
+	}
+}
+
+
+/**
+ * bubble_sort - This function sorts an array of size 'size'
  * in ascending order
  *
  * @array: An array to be sorted
@@ -11,18 +47,33 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	
 	int tmp;
+	size_t i, j, k;
 
-	for (size_t i = 0; i < size - 1; i++)
-       	{
-		for (size_t j = 0; j < size - i - 1; j++)
-	       	{
-            		if (array[j] > array[j + 1])
-		       	{
-                		tmp = array[j];
-                		array[j] = array[j + 1];
-                		array[j + 1] = tmp;
+	if (size < 2)
+	{
+		
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				for (k = 0; k < size; k++)
+				{
+					print_array_elmt(array[k]);
+					if (k != size - 1)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				putchar('\n');
 			}
 		}
 	}
--- sort.h
+++ sort.h
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
@@ -18,20 +32,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
+void print_array_elmt(int elmt);
 
 #endif
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
